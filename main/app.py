5c7f3174505e2f6dfc9e42912572b3d3617b4138
Albert Degenaar <albert.w.degenaar@accenture.com>
2020-09-23 13:12:04 -0400

    First working version

--- main/app.py
+++ main/app.py
@@ -8,39 +8,42 @@ import config
 
 def download_and_install_update_if_available():
     # read the credentials from the RTC
-
-    # import machine
-    # rtc = machine.RTC()
-    # rtc.memory(b'hello')
-
-#    o = OTAUpdater("https://github.com/adegenaar/MicroPythonWeather")
-#    o.download_and_install_update_if_available(config.SSID, config.password)
+    #    o = OTAUpdater("https://github.com/adegenaar/MicroPythonWeather")
+    #    o.download_and_install_update_if_available(config.SSID, config.password)
     pass
 
 def boot():
     download_and_install_update_if_available()
     # load the NTP time
     # see if the RTC needs to be updated...
+    import machine
+    rtc = machine.RTC()
 
+    # synchronize with ntp
+    # need to be connected to wifi
+    import ntptime
+    ntptime.settime() # set the rtc datetime from the remote server
+    #print (rtc.datetime())  
 
 def main():
     # appid = os.environ.get("OPENWEATHERMAP_APPID")
-    weather = Weather.weather()
+    weather = Weather.weather(300)
     effects = animations.animations()
     effects.setcolor(animations.black)
 
     radar = Radar.RCWL_0516()
     radar.begin(15)
-    timer = time.time() - float(3600)  # start with the timer off
+    timer = time.time()   # start with the timer off
     while True:
-        # if the radar is detecting a person, set the timer for 15 minutes
+        # if the radar is detecting a person, set the timer for 5 minutes
         if radar.isOn():
-            timer = time.time() + float(3600 * 15)  # 15 minutes
+            timer = time.time() # reset the timer
+            print ("Radar ON")
         else:
             pass
 
         # if the timer has expired, sleep and wait for the radar to come on
-        if time.time() > timer:
+        if time.time() - timer > (60 * 5): #5 minutes
             if effects.np[0] != animations.black:
                 effects.setcolor(animations.black)
             time.sleep_ms(50)
